549c0d887f1a074ba207d1eb3916e856cc66b95b
ChrisferDev <ferchris82@hotmail.com>
2024-04-22 16:03:58 -0500

    Versión 1.1

--- src/main/java/bancoApp/controladores/cliente/DashboardControlador.java
+++ src/main/java/bancoApp/controladores/cliente/DashboardControlador.java
@@ -1,4 +1,20 @@
 package bancoApp.controladores.cliente;
 
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+
 public class DashboardControlador {
+    public Text user_name;
+    public Label login_date;
+    public Label checking_bal;
+    public Label checking_acc_num;
+    public Label savings_bal;
+    public Label savings_acc_num;
+    public Label income_lbl;
+    public Label expense_lbl;
+    public ListView transaction_listview;
+    public TextField payee_fld;
+    public TextField amount_fld;
+    public TextArea message_fld;
+    public Button send_money_btn;
 }
