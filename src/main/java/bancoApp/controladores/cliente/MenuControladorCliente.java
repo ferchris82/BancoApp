c2d0f5119cc2ab5e766a1f4945253d8c2f9970c9
ChrisferDev <ferchris82@hotmail.com>
2024-04-22 13:34:14 -0500

    Versión 1.2

--- src/main/java/bancoApp/controladores/cliente/MenuControladorCliente.java
+++ src/main/java/bancoApp/controladores/cliente/MenuControladorCliente.java
@@ -1,5 +1,13 @@
 package bancoApp.controladores.cliente;
 
+import javafx.scene.control.Button;
+
 public class MenuControladorCliente {
-    
+
+    public Button dashboard_btn;
+    public Button transaction_btn;
+    public Button accounts_btn;
+    public Button profile_btn;
+    public Button logout_btn;
+    public Button report_btn;
 }
