2344078ec2aece779f7a0c941afdb66d09147f93
ChrisferDev <ferchris82@hotmail.com>
2024-04-23 21:17:10 -0500

    Version 1.3

---
+++ src/main/java/bancoApp/controladores/admin/ControladorMenuAdministrador.java
@@ -0,0 +1,19 @@
+package bancoApp.controladores.admin;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControladorMenuAdministrador implements Initializable {
+    public Button create_client_btn;
+    public Button clients_btn;
+    public Button deposit_btn;
+    public Button logout_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+    }
+}
--- src/main/java/bancoApp/controladores/cliente/CuentasControlador.java
+++ src/main/java/bancoApp/controladores/cliente/CuentasControlador.java
@@ -24,6 +24,6 @@ public class CuentasControlador implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        
+
     }
 }
