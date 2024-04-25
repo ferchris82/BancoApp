44330555a7ae2c50969b034c085e8fb05f8f560b
ChrisferDev <ferchris82@hotmail.com>
2024-04-25 13:17:03 -0500

    Versión 1.4

---
+++ src/main/java/bancoApp/controladores/admin/ControladorClienteMovil.java
@@ -0,0 +1,23 @@
+package bancoApp.controladores.admin;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControladorClienteMovil implements Initializable {
+    public Label fName_lbl;
+    public Label lName_lbl;
+    public Label pAddress_lbl;
+    public Label ch_acc_lbl;
+    public Label sv_acc_lbl;
+    public Label date_lbl;
+    public Button delete_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
