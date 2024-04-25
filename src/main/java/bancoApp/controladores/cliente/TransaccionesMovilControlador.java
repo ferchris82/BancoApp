048e48e153a54254cd8b45abbd9bcc7bfad3840c
ChrisferDev <ferchris82@hotmail.com>
2024-04-25 12:39:15 -0500

    Versión 1.4

---
+++ src/main/java/bancoApp/controladores/cliente/TransaccionesMovilControlador.java
@@ -0,0 +1,22 @@
+package bancoApp.controladores.cliente;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TransaccionesMovilControlador implements Initializable {
+    public FontAwesomeIconView in_icon;
+    public FontAwesomeIconView out_icon;
+    public Label trans_date_lbl;
+    public Label sender_lbl;
+    public Label receiver_lbl;
+    public Label amount_lbl;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
